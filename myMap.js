e9e4c77f139a387a4381ab0119c8ae69055b5e7d
Regina <reginazhang14@gmail.com>
2017-12-14 13:53:41 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -14,7 +14,7 @@ map_manager.map_items = [
       "pokemon_id" : 27,
       "expire" : 1513600358,
       "longitude" : -111.862240,
-      "latitude" : 32.7596651,
+      "latitude" : 33.289580,
     }
 ]
 
@@ -23,12 +23,13 @@ function loadMapScenario() {
     for (var i in map_manager.map_items){
         var map_item = map_manager.map_items[i];
         var iconUrl = 'https://raw.githubusercontent.com/chenditc-bittiger/pokemon_week3_fronend/master/images/pushpin_images/pokemon/'+ map_item['pokemon_id'] + '.png';
-        //console.log(iconUrl);
+        console.log(iconUrl);
         var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
                                                  //{ title: get_counter_down_time_from_expire_epoch(map_item['expire']), 
                                                  {icon: iconUrl});
+        map.entities.push(pushpin);
     }
-    map.entities.push(pushpin);
+    
 }
     
 
