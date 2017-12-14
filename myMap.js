60ebd8bbe2d2b2d9e78563d62410153fc38773b9
Regina <reginazhang14@gmail.com>
2017-12-14 13:33:40 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -24,7 +24,7 @@ function loadMapScenario() {
         var map_item = map_manager.map_items[i];
         var iconUrl = 'https://raw.githubusercontent.com/chenditc-bittiger/pokemon_week3_fronend/master/images/pushpin_images/pokemon/'+ map_item['pokemon_id'] + '.png';
         console.log(iconUrl);
-        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
+        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"])), 
                                                  {icon: iconUrl});
         map.entities.push(pushpin);
     }
