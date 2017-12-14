3ae1e9dcf2f0ccc137ca1a136a09c9153711b10b
Regina <reginazhang14@gmail.com>
2017-12-14 13:10:16 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,3 +1,7 @@
 function loadMapScenario() {
- var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://raw.githubusercontent.com/chenditc-bittiger/pokemon_week3_fronend/master/images/pushpin_images/pokemon/2.png',
+        anchor: new Microsoft.Maps.Point(12, 39) });
+    map.entities.push(pushpin);
+
 }
