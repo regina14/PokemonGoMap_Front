f7f0b6dc9c16a328ec788f1379e013bbf75d5f64
Regina <reginazhang14@gmail.com>
2017-12-14 13:46:57 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -7,14 +7,14 @@ map_manager.map_items = [
     {
       "pokemon_id" : 12,
       "expire" : 1513290358,
-      "longitude" : -60.9800345,
-      "latitude" : 40.7596651,
+      "longitude" : -111.862240,
+      "latitude" : 34.7596651,
     },
     {
       "pokemon_id" : 2,
       "expire" : 1513600358,
-      "longitude" : -70.9800123,
-      "latitude" : 41.7596651,
+      "longitude" : -111.862240,
+      "latitude" : 32.7596651,
     }
 ]
 
@@ -24,8 +24,8 @@ function loadMapScenario() {
         var map_item = map_manager.map_items[i];
         var iconUrl = 'https://raw.githubusercontent.com/chenditc-bittiger/pokemon_week3_fronend/master/images/pushpin_images/pokemon/'+ map_item['pokemon_id'] + '.png';
         //console.log(iconUrl);
-        var pushpin = new Microsoft.Maps.Pushpin(//new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"])), 
-                                                 map.getCenter(),{icon: iconUrl});
+        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"])), 
+                                                 {icon: iconUrl});
         map.entities.push(pushpin);
     }
     
