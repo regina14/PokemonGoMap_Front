1d3f9e0e866ba7a8787ca5315c7d34f8fef62b5d
Regina <reginazhang14@gmail.com>
2017-12-14 13:22:04 -0700

    add different icon pictures

--- myMap.js
+++ myMap.js
@@ -1,7 +1,32 @@
+var map_manager = {
+    "map" : null,
+    "map_items" : []
+}
+
+map_manager.map_items = [
+    {
+      "pokemon_id" : 15,
+      "expire" : 1513290358,
+      "longitude" : 33.28,
+      "latitude" : -111.86,
+    },
+    {
+      "pokemon_id" : 3,
+      "expire" : 1513600358,
+      "longitude" : 34.928,
+      "latitude" : -111.86,
+    }
+]
+
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://raw.githubusercontent.com/chenditc-bittiger/pokemon_week3_fronend/master/images/pushpin_images/pokemon/2.png',
-        anchor: new Microsoft.Maps.Point(12, 39) });
-    map.entities.push(pushpin);
+    for (var i in map_manager.map_items){
+        var map_item = map_manager.map_items[i];
+        var iconUrl = 'https://raw.githubusercontent.com/chenditc-bittiger/pokemon_week3_fronend/master/images/pushpin_images/pokemon/'+ map_item['pokemon_id'] + '.png';
+        var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: iconUrl
+                                                               );
+        map.entities.push(pushpin);
+    }
+    
 
 }
